ba62d76d2e19f36590ae68138725c710c7ca373d
pepoblossom <85858096+pepoblossom@users.noreply.github.com>
2021-06-14 16:09:58 +0900

    Add files via upload

---
+++ ボタン音.js
@@ -0,0 +1,17 @@
+
+
+(function (window, $) {
+  'use strict';
+ 
+  $.fn.useSound = function (_event, _id) {
+    var se = $(_id);
+    this.on(_event, function(){
+      se[0].currentTime = 0;
+      se[0].play();
+    });
+    return this;
+  };
+ 
+})(this, this.jQuery);
+ 
+$('.TV').useSound('mousedown touchstart', '#sound');
